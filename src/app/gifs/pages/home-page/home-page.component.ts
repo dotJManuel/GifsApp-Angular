293d7128f98320b65e9f171c333a5e2d80b1a33c
dotJManuel <dotJManuel@outlook.com>
2023-11-26 20:07:39 -0600

    se agrego la conexion de la api Giphy

--- src/app/gifs/pages/home-page/home-page.component.ts
+++ src/app/gifs/pages/home-page/home-page.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
+import { Gif } from '../../interfaces/gifs.interfaces';
 
 @Component({
   selector: 'gifs-home-page',
   templateUrl: './home-page.component.html'
 })
-export class HomePageComponent implements OnInit {
+export class HomePageComponent {
 
-  constructor() { }
+  constructor( private gifsService: GifsService ) { }
 
-  ngOnInit() {
+  get gifs(): Gif[] {
+    return this.gifsService.gifList;
   }
-
 }
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,12 +1,18 @@
 
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
 
 @Injectable({providedIn: 'root'})
 export class GifsService {
 
-  private _tagsHistory: string[] = [];
+  public gifList: Gif[] = [];
 
-  constructor() { }
+  private _tagsHistory: string[] = [];
+  private apiKey: string = 'vxBlWTU2eYPIdSARNrqAeU73BzuAeGoi';
+  private serviceURL: string = 'https://api.giphy.com/v1/gifs';
+
+  constructor( private http: HttpClient ) { }
 
   get tagHistory() {
     return [...this._tagsHistory];
@@ -25,8 +31,19 @@ export class GifsService {
 
   searchTag( tag: string ):void {
     if(tag.length === 0) return;
-
     this.organizeHistory(tag);
+
+    const params = new HttpParams()
+      .set( 'api_key', this.apiKey )
+      .set( 'limit', '10' )
+      .set( 'q', tag );
+
+    this.http.get<SearchResponse>(`${ this.serviceURL }/search`, { params })
+      .subscribe( resp => {
+
+        this.gifList = resp.data;
+
+      });
   }
 
 }
