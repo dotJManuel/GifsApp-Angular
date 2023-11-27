8971d3c7f168c9c67468f3f78e882198a1937090
dotJManuel <dotJManuel@outlook.com>
2023-11-26 21:21:38 -0600

    se agrego para guardar en localstorage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -12,7 +12,9 @@ export class GifsService {
   private apiKey: string = 'vxBlWTU2eYPIdSARNrqAeU73BzuAeGoi';
   private serviceURL: string = 'https://api.giphy.com/v1/gifs';
 
-  constructor( private http: HttpClient ) { }
+  constructor( private http: HttpClient ) {
+    this.loadLocalStorage();
+   }
 
   get tagHistory() {
     return [...this._tagsHistory];
@@ -27,6 +29,22 @@ export class GifsService {
 
     this._tagsHistory.unshift( tag );
     this._tagsHistory = this._tagsHistory.splice(0, 10);
+
+    this.saveLocalStorage();
+  }
+
+  private saveLocalStorage(): void {
+    localStorage.setItem('history', JSON.stringify( this._tagsHistory ));
+  }
+
+  private loadLocalStorage():void {
+
+    if( !localStorage.getItem('history') ) return;
+
+    this._tagsHistory = JSON.parse( localStorage.getItem('history')! );
+
+    if( this._tagsHistory.length === 0 ) return;
+    this.searchTag( this._tagsHistory[0] );
   }
 
   searchTag( tag: string ):void {
@@ -35,7 +53,7 @@ export class GifsService {
 
     const params = new HttpParams()
       .set( 'api_key', this.apiKey )
-      .set( 'limit', '10' )
+      .set( 'limit', '12' )
       .set( 'q', tag );
 
     this.http.get<SearchResponse>(`${ this.serviceURL }/search`, { params })
